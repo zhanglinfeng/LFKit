456169fea901e079cf881bf1cc0160d8e96a2a9f
张林峰 <1051034428@qq.com>
2018-02-02 15:18:16 +0800

    日志组件调整

--- LFKit/LFKit/Component/LFLogManager/LFLogManager.m
+++ LFKit/LFKit/Component/LFLogManager/LFLogManager.m
@@ -75,78 +75,6 @@ DDLogLevel ddLogLevel = DDLogLevelError;
 
 
 
-#pragma mark - 日志管理
-@interface LFLogManager ()
-
-@property (nonatomic, strong) DDFileLogger *fileLogger;
-
-@end
-
-@implementation LFLogManager
-
-
-+ (instancetype)shareInstance {
-    static id shareInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        shareInstance = [[[self class] alloc]init];
-    });
-    return shareInstance;
-}
-
-+ (void)install {
-    BOOL isSetLevel = [[NSUserDefaults standardUserDefaults] boolForKey:isSetLevelKey];
-    if (isSetLevel) {
-        ddLogLevel = [[NSUserDefaults standardUserDefaults] integerForKey:DefaultLogLevelKey];
-    }
-    
-    [[LFLogManager shareInstance] configureLog];
-//    [LFExceptionHandler installExceptionHandler];
-}
-
-- (void)configureLog
-{
-#ifdef DEBUG
-    //发送日志语句到苹果的日志系统，以便它们显示在Console.app上
-//    [DDLog addLogger:[DDASLLogger sharedInstance] withLevel:DDLogLevelDebug];
-    //发送日志语句到Xcode控制台
-    [DDLog addLogger:[DDTTYLogger sharedInstance]];
-#endif
-    
-    // 允许颜色
-    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
-    //自定义log格式
-    [DDTTYLogger sharedInstance].logFormatter = [[LFLogFormatter alloc] init];
-    //log文件
-    [DDLog addLogger:self.fileLogger withLevel:DDLogLevelAll];
-}
-
-- (NSArray *)getLogFiles {
-    return self.fileLogger.logFileManager.sortedLogFileInfos;
-}
-
-- (void)setLogLevel:(NSInteger)level {
-    ddLogLevel = level;
-    [[NSUserDefaults standardUserDefaults] setInteger:ddLogLevel forKey:DefaultLogLevelKey];
-    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:isSetLevelKey];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    
-}
-
-- (DDFileLogger *)fileLogger
-{
-    if (!_fileLogger) {
-        DDFileLogger *fileLogger = [[DDFileLogger alloc] init];
-        fileLogger.rollingFrequency = 60 * 60 * 24; // 24 hour rolling
-        fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
-        fileLogger.maximumFileSize = 1024*1024;
-        _fileLogger = fileLogger;
-    }
-    
-    return _fileLogger;
-}
-
-@end
 
 
 
@@ -266,3 +194,82 @@ void SignalHandler(int signal) {
 }
 
 @end
+
+
+
+
+
+
+#pragma mark - 日志管理
+@interface LFLogManager ()
+
+@property (nonatomic, strong) DDFileLogger *fileLogger;
+
+@end
+
+@implementation LFLogManager
+
+
++ (instancetype)shareInstance {
+    static id shareInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shareInstance = [[[self class] alloc]init];
+    });
+    return shareInstance;
+}
+
++ (void)install {
+    BOOL isSetLevel = [[NSUserDefaults standardUserDefaults] boolForKey:isSetLevelKey];
+    if (isSetLevel) {
+        ddLogLevel = [[NSUserDefaults standardUserDefaults] integerForKey:DefaultLogLevelKey];
+    }
+    
+    [[LFLogManager shareInstance] configureLog];
+    [LFExceptionHandler installExceptionHandler];
+}
+
+- (void)configureLog
+{
+#ifdef DEBUG
+    //发送日志语句到苹果的日志系统，以便它们显示在Console.app上
+//    [DDLog addLogger:[DDASLLogger sharedInstance] withLevel:DDLogLevelDebug];
+    //发送日志语句到Xcode控制台
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+#endif
+    
+    // 允许颜色
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    //自定义log格式
+    [DDTTYLogger sharedInstance].logFormatter = [[LFLogFormatter alloc] init];
+    //log文件
+    [DDLog addLogger:self.fileLogger withLevel:DDLogLevelAll];
+}
+
+- (NSArray *)getLogFiles {
+    return self.fileLogger.logFileManager.sortedLogFileInfos;
+}
+
+- (void)setLogLevel:(NSInteger)level {
+    ddLogLevel = level;
+    [[NSUserDefaults standardUserDefaults] setInteger:ddLogLevel forKey:DefaultLogLevelKey];
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:isSetLevelKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+}
+
+- (DDFileLogger *)fileLogger
+{
+    if (!_fileLogger) {
+        DDFileLogger *fileLogger = [[DDFileLogger alloc] init];
+        fileLogger.rollingFrequency = 60 * 60 * 24; // 24 hour rolling
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
+        fileLogger.maximumFileSize = 1024*1024;
+        _fileLogger = fileLogger;
+    }
+    
+    return _fileLogger;
+}
+
+@end
+
